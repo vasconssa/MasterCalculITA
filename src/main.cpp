2c2122cad4d1452c06e924966f850129ae25997c
Felipe Bomfim <felipebomfim4161@gmail.com>
2019-12-02 03:48:19 -0300

    Modificao

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include "Interface.h"
 #include <QApplication>
+#include <logindialog.h>
 
 int main(int argc, char *argv[]) {
 	QApplication a(argc, argv);
-	Interface interface;
-	interface.show();
+//	Interface interface;
+//	interface.show();
+
+    LoginDialog ld;
+
+    ld.show();
+
 
 	return a.exec();
 }
